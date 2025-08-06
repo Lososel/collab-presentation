348e0ca8aad971dc32edce09692ef64fe928222f
Elmira Shainurova <shainurova.nursultan@gmail.com>
2025-08-07 02:35:50 +0500

    feat: add database config

---
+++ server/src/index.ts
@@ -0,0 +1,12 @@
+import db from '../src/config/db.js'
+
+async function getUsers() {
+  try {
+    const result = await db.query('SELECT * FROM users');
+    console.log(result.rows);
+  } catch (err) {
+    console.error('Error executing query', err);
+  }
+}
+
+getUsers();
