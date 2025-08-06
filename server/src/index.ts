7d3d401b7ca90a20ef02a3e59160ddb0dfcda275
Elmira Shainurova <shainurova.nursultan@gmail.com>
2025-08-07 03:16:57 +0500

    feat: add basic router

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,30 @@
-import db from '../src/config/db.js';
+import express from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+import cors from 'cors';
+import { handleUserSocket } from './sockets/user.socket.js';
+import appRoutes from './routes/AppRouter.js';
 
-async function getUsers() {
-  try {
-    const result = await db.query('SELECT * FROM users');
-    console.log(result.rows);
-  } catch (err) {
-    console.error('Error executing query', err);
-  }
-}
+const app = express();
+const server = http.createServer(app);
 
-getUsers();
+const io = new Server(server, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST'],
+  },
+});
+
+app.use(cors());
+app.use(express.json());
+app.use('/', appRoutes);
+
+io.on('connection', (socket) => {
+  console.log('Socket connected:', socket.id);
+  handleUserSocket(io, socket);
+});
+
+const PORT = process.env.PORT || 5005;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
